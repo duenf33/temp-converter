54f1c77b3ef8103973823e530c417f1b222d73df
fernando duenas <fernando39615@aim.com>
2020-10-21 13:38:25 -0500

    Initial commit.

---
+++ c2f.js
@@ -0,0 +1,7 @@
+const convert = function(temp){
+    return temp * 9 / 5 + 32;
+}
+// const result = convert(20)
+// result;
+
+module.exports = convert;
---
+++ f2c.js
@@ -0,0 +1,8 @@
+const convert = function(temp){
+    return (temp - 32) * 5 / 9; 
+}
+// const result = convert(80)
+// result;
+
+module.exports = convert;
+
